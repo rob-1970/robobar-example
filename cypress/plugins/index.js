e8e448da383639187008012b6e177c0b38372ba6
rob-1970 <rob.unic.1970@gmail.com>
2022-02-16 13:11:32 +0100

    FIST Google TEST using CUCUMBER in CYPRESS

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -12,7 +12,6 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-// https://github.com/TheBrainFamily/cypress-cucumber-preprocessor
 const cucumber = require('cypress-cucumber-preprocessor').default
 
 /**
